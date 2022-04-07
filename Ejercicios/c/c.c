e14af82e2b83947a5f5a7ac35acc7208d7000d22
Gianfranco Talocchino <gianfranco.talocchino@cintelink.com.ar>
2022-04-07 09:30:40 -0300

    Agregado ejercicio 4 de la guia

--- Ejercicios/c/c.c
+++ Ejercicios/c/c.c
@@ -27,3 +27,13 @@ void dot_product_16(uint16_t *in, uint16_t *out, uint32_t size, uint16_t k) {
    }
 }
 
+void dot_product_12(uint16_t *in, uint16_t *out, uint32_t size, uint16_t k) {
+   for (uint32_t i = 0; i < size; i++) {
+      out[i] = k * in[i];
+
+      if (out[i] > 0xfff) {
+         out[i] = 0xfff;
+      }
+   }
+}
+
