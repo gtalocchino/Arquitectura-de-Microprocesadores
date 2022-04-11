1ac2d6699690a00dd422f7a31139d3c091c4a170
Gianfranco Talocchino <gftalocchino@gmail.com>
2022-04-10 21:37:15 -0300

    Se agregran archivos para las funciones en asm

---
+++ Ejercicios/asm/asm.h
@@ -0,0 +1,58 @@
+/*
+ * c.h
+ *
+ *  Created on: Apr 6, 2022
+ *      Author: gianfranco
+ */
+
+#ifndef C_H_
+#define C_H_
+
+
+#include "inttypes.h"
+
+
+/**
+ * @brief Resets an input array of 32-bit integers to zero.
+ *
+ * @param[in] p Pointer to the array.
+ * @param[in] size Size of the array.
+ *
+ */
+void asm_zeros(uint32_t *p, uint32_t size);
+
+/**
+ * @brief Performs dot product with 32-bit unsigned integers.
+ *
+ * @param[in] in Pointer to the input array 
+ * @param[out] out Pointer to the output array
+ * @param[in] size Array size
+ * @param[in] k Scalar value
+ *
+ */
+void asm_dot_product_32(uint32_t *in, uint32_t *out, uint32_t size, uint32_t k);
+
+/**
+ * @brief Performs dot product with 16-bit unsigned integers.
+ *
+ * @param[in] in Pointer to the input array 
+ * @param[out] out Pointer to the output array
+ * @param[in] size Array size
+ * @param[in] k Scalar value
+ *
+ */
+void asm_dot_product_16(uint16_t *in, uint16_t *out, uint32_t size, uint16_t k);
+
+/**
+ * @brief Performs dot product Saturating the result to 12 bits.
+ *
+ * @param[in] in Pointer to the input array 
+ * @param[out] out Pointer to the output array
+ * @param[in] size Array size
+ * @param[in] k Scalar value
+ *
+ */
+void asm_dot_product_12(uint16_t *in, uint16_t *out, uint32_t size, uint16_t k);
+
+#endif /* C_H_ */
+
