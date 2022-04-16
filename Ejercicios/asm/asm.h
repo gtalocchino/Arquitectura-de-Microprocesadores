9fd2fd1a85f73464fa683178848868543ad4ec63
Gianfranco Talocchino <gftalocchino@gmail.com>
2022-04-16 17:42:47 -0300

    Se argrega ejericio 7 en asm

--- Ejercicios/asm/asm.h
+++ Ejercicios/asm/asm.h
@@ -73,5 +73,15 @@ void asm_window_filter(uint16_t *in, uint16_t *out, uint32_t size);
  */
 void asm_pack_32_to_16(int32_t *in, int16_t *out, uint32_t size);
 
+/**
+ * @brief Returns the index of maximum.
+ *
+ * @param[in] in Pointer to the input array
+ * @param[in] size Array size
+ *
+ * @retval Index of maximum
+ */
+int32_t asm_max(int32_t *in, uint32_t size);
+
 #endif /* INC_ASM_H_ */
 
